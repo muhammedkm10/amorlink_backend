69a73800bb394d4462b75c58871db01ddd5be2c1
muhammed <muhammedmamu2906@gmail.com>
2024-07-09 16:46:27 +0530

    allowed host

--- backend/settings.py
+++ backend/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-q2at82iwysz7o!=(wz!$lkpazl4x7bbij+t_n1=*=eomcb6kpp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.201.26.194']
 
 
 # Application definition
