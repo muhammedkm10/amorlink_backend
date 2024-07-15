c48f97228e1d9dd818f4aea16cc0dbb64518b9d5
muhammed <muhammedmamu2906@gmail.com>
2024-07-15 17:55:10 +0530

    asgin changed

--- backend/asgi.py
+++ backend/asgi.py
@@ -2,10 +2,10 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chatapp.routing import websocket_urlpatterns
+application = get_asgi_application()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-application = get_asgi_application()
 
 application = ProtocolTypeRouter({
     'http': application,
--- backend/wsgi.py
+++ backend/wsgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
