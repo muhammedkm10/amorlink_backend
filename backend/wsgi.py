4915b2729662191726a0b949afff3da8e5cc50d5
muhammed <muhammedmamu2906@gmail.com>
2024-07-15 17:50:19 +0530

    wsgi file

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
