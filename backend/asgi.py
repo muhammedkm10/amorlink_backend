faecab973b3c14ca5f819d3b55ed2d42f78c57c1
muhammed <muhammedmamu2906@gmail.com>
2024-07-15 20:31:11 +0530

    changed

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,9 +1,9 @@
 import os
 from django.core.asgi import get_asgi_application
-application = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chatapp.routing import websocket_urlpatterns
+application = get_asgi_application()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
