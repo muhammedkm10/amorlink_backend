19d56d0ddc5ceed86c718a22157ae86b65da74ce
muhammed <muhammedmamu2906@gmail.com>
2024-07-16 16:22:08 +0530

    changed

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,13 +1,18 @@
-
-from django.core.asgi import get_asgi_application
 import os
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from chatapp.routing import websocket_urlpatterns
-application = get_asgi_application()
+from django.apps import apps
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+# Import websocket_urlpatterns only after Django is set up
+from chatapp.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter({
-    'http': application,
-    'websocket': URLRouter(websocket_urlpatterns),
+    "http": django_asgi_app,
+    "websocket": URLRouter(websocket_urlpatterns),
 })
